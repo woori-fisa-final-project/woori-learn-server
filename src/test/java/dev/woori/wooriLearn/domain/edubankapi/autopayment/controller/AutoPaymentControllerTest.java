a915cdb5f06c8b14422c0778117e1145aff3e2cb
dalhana8 <mwk0808@gmail.com>
2025-11-12 10:49:56 +0900

    [fix] 필터 체인 변경

--- src/test/java/dev/woori/wooriLearn/domain/edubankapi/autopayment/controller/AutoPaymentControllerTest.java
+++ src/test/java/dev/woori/wooriLearn/domain/edubankapi/autopayment/controller/AutoPaymentControllerTest.java
@@ -32,7 +32,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
-class gitAutoPaymentControllerTest {
+class AutoPaymentControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
