aaa59fa1e41bfda7b36e2a1db016d8a781fac79a
poul0102 <lday217617@gmail.com>
2025-11-17 17:29:55 +0900

    [refactor] @EntityGraph 어노테이션 활용

--- src/main/java/dev/woori/wooriLearn/domain/scenario/repository/ScenarioCompletedRepository.java
+++ src/main/java/dev/woori/wooriLearn/domain/scenario/repository/ScenarioCompletedRepository.java
@@ -3,15 +3,14 @@ package dev.woori.wooriLearn.domain.scenario.repository;
 import dev.woori.wooriLearn.domain.scenario.entity.Scenario;
 import dev.woori.wooriLearn.domain.scenario.entity.ScenarioCompleted;
 import dev.woori.wooriLearn.domain.user.entity.Users;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ScenarioCompletedRepository extends JpaRepository<ScenarioCompleted, Long> {
     boolean existsByUserAndScenario(Users user, Scenario scenario);
 
-    @Query("SELECT sc FROM ScenarioCompleted sc JOIN FETCH sc.scenario WHERE sc.user = :user")
-    List<ScenarioCompleted> findByUser(@Param("user") Users user);
+    @EntityGraph(attributePaths = "scenario")
+    List<ScenarioCompleted> findByUser(Users user);
 }
