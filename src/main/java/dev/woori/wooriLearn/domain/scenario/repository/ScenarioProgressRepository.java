638ea46a8672ce687db0d2590303d56a5d1fe7a4
poul0102 <lday217617@gmail.com>
2025-11-17 17:21:28 +0900

    [refactor] N+1 문제 개선/구조 개선(사용자 조회 로직 서비스 계층으로 이전

--- src/main/java/dev/woori/wooriLearn/domain/scenario/repository/ScenarioProgressRepository.java
+++ src/main/java/dev/woori/wooriLearn/domain/scenario/repository/ScenarioProgressRepository.java
@@ -4,6 +4,8 @@ import dev.woori.wooriLearn.domain.scenario.entity.Scenario;
 import dev.woori.wooriLearn.domain.scenario.entity.ScenarioProgress;
 import dev.woori.wooriLearn.domain.user.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,5 +13,6 @@ import java.util.Optional;
 public interface ScenarioProgressRepository extends JpaRepository<ScenarioProgress, Long> {
     Optional<ScenarioProgress> findByUserAndScenario(Users user, Scenario scenario);
 
-    List<ScenarioProgress> findByUser(Users user);
+    @Query("SELECT sp FROM ScenarioProgress sp JOIN FETCH sp.scenario WHERE sp.user = :user")
+    List<ScenarioProgress> findByUser(@Param("user") Users user);
 }
