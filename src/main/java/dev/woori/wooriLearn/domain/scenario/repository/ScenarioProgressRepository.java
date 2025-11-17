0f0b14f20731ed71fd47d00a8ea935126c4caa21
poul0102 <lday217617@gmail.com>
2025-11-17 17:29:55 +0900

    [refactor] @EntityGraph 어노테이션 활용

--- src/main/java/dev/woori/wooriLearn/domain/scenario/repository/ScenarioProgressRepository.java
+++ src/main/java/dev/woori/wooriLearn/domain/scenario/repository/ScenarioProgressRepository.java
@@ -3,9 +3,8 @@ package dev.woori.wooriLearn.domain.scenario.repository;
 import dev.woori.wooriLearn.domain.scenario.entity.Scenario;
 import dev.woori.wooriLearn.domain.scenario.entity.ScenarioProgress;
 import dev.woori.wooriLearn.domain.user.entity.Users;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,6 +12,6 @@ import java.util.Optional;
 public interface ScenarioProgressRepository extends JpaRepository<ScenarioProgress, Long> {
     Optional<ScenarioProgress> findByUserAndScenario(Users user, Scenario scenario);
 
-    @Query("SELECT sp FROM ScenarioProgress sp JOIN FETCH sp.scenario WHERE sp.user = :user")
-    List<ScenarioProgress> findByUser(@Param("user") Users user);
+    @EntityGraph(attributePaths = "scenario")
+    List<ScenarioProgress> findByUser(Users user);
 }
