71d746c775930a644a8b4e7566b88dc9af5b417f
kimhm0828 <kimhm0828@gmail.com>
2025-11-20 10:02:37 +0900

    [feat] 비밀번호 검증 로직 추가

---
+++ src/main/java/dev/woori/wooriLearn/domain/auth/dto/util/PasswordValidator.java
@@ -0,0 +1,21 @@
+package dev.woori.wooriLearn.domain.auth.dto.util;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.regex.Pattern;
+
+public class PasswordValidator implements ConstraintValidator<ValidPassword, String> {
+    // 8~20자 / 영문자 1개 이상 / 숫자 1개 이상 / 특수문자 1개 이상
+    private static final String PASSWORD_REGEX =
+            "^(?=.{8,20}$)(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-={}\\[\\]|\\\\:;\"'<>,.?/]).+$";
+
+    private final Pattern pattern = Pattern.compile(PASSWORD_REGEX);
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (value == null) return false;
+        return pattern.matcher(value).matches();
+    }
+
+}
