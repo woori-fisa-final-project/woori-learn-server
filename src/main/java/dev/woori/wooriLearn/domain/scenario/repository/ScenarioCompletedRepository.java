5dda54056d2b9605ad70c0c307765ea9bfc6c740
kimhm0828 <kimhm0828@gmail.com>
2025-11-20 15:09:17 +0900

    [refactor] n+1 문제 해결을 위한 쿼리 수정

--- src/main/java/dev/woori/wooriLearn/domain/scenario/repository/ScenarioCompletedRepository.java
+++ src/main/java/dev/woori/wooriLearn/domain/scenario/repository/ScenarioCompletedRepository.java
@@ -5,6 +5,8 @@ import dev.woori.wooriLearn.domain.scenario.entity.ScenarioCompleted;
 import dev.woori.wooriLearn.domain.user.entity.Users;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -14,5 +16,11 @@ public interface ScenarioCompletedRepository extends JpaRepository<ScenarioCompl
     @EntityGraph(attributePaths = "scenario")
     List<ScenarioCompleted> findByUser(Users user);
 
-    long countByUserId(Long userId);
+    @Query("""
+        SELECT sc.user.id AS userId, COUNT(sc) AS completedCount
+        FROM ScenarioCompleted sc
+        WHERE sc.user.id IN :userIds
+        GROUP BY sc.user.id
+    """)
+    List<ScenarioCompletedCount> countCompletedByUserIds(@Param("userIds") List<Long> userIds);
 }
