8179991d73bdd12b8967e2c723f0d798fd3928d4
junn34 <lucasorjh3412@gmail.com>
2025-11-23 12:53:50 +0900

    [feat] 관리자 기능 전체 회원 환전신청 내역 조회 api 추가

--- src/main/java/dev/woori/wooriLearn/domain/account/repository/PointsHistoryRepository.java
+++ src/main/java/dev/woori/wooriLearn/domain/account/repository/PointsHistoryRepository.java
@@ -1,11 +1,16 @@
 package dev.woori.wooriLearn.domain.account.repository;
 
 import dev.woori.wooriLearn.domain.account.entity.PointsHistory;
+import dev.woori.wooriLearn.domain.account.entity.PointsHistoryType;
+import dev.woori.wooriLearn.domain.account.entity.PointsStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import jakarta.persistence.LockModeType;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
@@ -14,5 +19,6 @@ public interface PointsHistoryRepository extends JpaRepository<PointsHistory, Lo
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT h FROM PointsHistory h JOIN FETCH h.user WHERE h.id = :id")
     Optional<PointsHistory> findAndLockById(@Param("id") Long id);
-}
 
+    Page<PointsHistory> findByTypeAndStatus(PointsHistoryType type, PointsStatus status, Pageable pageable);
+}
