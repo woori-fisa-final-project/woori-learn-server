a3477ee2451afa1b6dc0ed6705aa0e0524db349f
kimhm0828 <kimhm0828@gmail.com>
2025-11-27 14:18:38 +0900

    [chore] 사용되지 않는 import문 삭제

--- src/main/java/dev/woori/wooriLearn/config/security/SecurityConfig.java
+++ src/main/java/dev/woori/wooriLearn/config/security/SecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
